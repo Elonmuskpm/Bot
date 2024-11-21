8abc9a19e7ed896d8d51d14ed67fbf877b35988c
Elonmuskpm <elonmuskteslasignal@gmail.com>
2024-11-21 06:12:04 +0100

    Update TelegramBotAPI.js

--- api/TelegramBotAPI.js
+++ api/TelegramBotAPI.js
@@ -2,7 +2,7 @@
  * Telegram API for: 
  *      - sendMessage
  *      - setMessageReaction
- * Repository: https://github.com/Malith-Rukshan/Auto-Reaction-Bot
+ * Repository: https://github.com/Elonmuskpm/Bot
  */
 import fetch from 'node-fetch';
 
